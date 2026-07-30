7b4666d714ee08612b53185e983ebffdf6b6271b
cockcut <105777311+cockcut@users.noreply.github.com>
2026-07-30 11:47:38 +0900

    Update radius_install.sh

--- radius_install.sh
+++ radius_install.sh
@@ -67,8 +67,10 @@ else
 fi
 
 # 데이터베이스와 사용자 생성
-echo "MySQL DB(${MYSQL_DATABASE})를 초기화합니다."
+echo "MySQL DB(${MYSQL_DATABASE})와 사용자(${MYSQL_USER})를 초기화합니다."
 sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "DROP DATABASE IF EXISTS \`${MYSQL_DATABASE}\`"
+sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "DROP USER \'${MYSQL_USER}\'@\'${MYSQL_HOST}\'"
+
 echo "MySQL DB(${MYSQL_DATABASE})를 다시 생성합니다."
 sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`"
 sudo mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'${MYSQL_HOST}' IDENTIFIED BY '${MYSQL_PASSWORD}';"
